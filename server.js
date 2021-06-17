5f8f4a3665ae3d346375a5546065192f82166ad7
or.llon <dhflxhdxhd@gmail.com>
2021-06-18 01:46:18 +0900

    ....?

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/',function(req, res) {
     //__dirname 은 요청하고자 하는 파일의 경로(최상위 디렉토리~현재 디렉토리)를 단축시켜주는 절대경로 식별자
     res.sendFile(__dirname + "/index.html");
 })
-const PORT = process.env.PORT || 8080 
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log("server is loading :)")
   })
