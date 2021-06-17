e29b30d33c9f1cb41a26f20702154725f07c1623
or.llon <dhflxhdxhd@gmail.com>
2021-06-18 01:40:33 +0900

    아니..왜? 포트가 연결이 안되네

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.get('/',function(req, res) {
     //__dirname 은 요청하고자 하는 파일의 경로(최상위 디렉토리~현재 디렉토리)를 단축시켜주는 절대경로 식별자
     res.sendFile(__dirname + "/index.html");
 })
-const PORT = process.env.PORT
-app.listen(PORT)
+const PORT = process.env.PORT || 8080 
+app.listen(PORT, () => {
+    console.log("server is loading :)")
+  })
 
 // app.listen(8080,()=>{
 //     console.log("server is loading :)");
