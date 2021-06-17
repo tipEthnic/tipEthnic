9a59d8df90178b32cb3d1b364d319790fbe78239
or.llon <dhflxhdxhd@gmail.com>
2021-06-18 04:43:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.get('/',function(req, res) {
     res.sendFile(__dirname + "/index.html");
 })
 
-
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
     console.log("server is loading :)");
 })
 
