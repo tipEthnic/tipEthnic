1e80e690f58c56cc7758a108f7a5c8085fa6e5eb
or.llon <dhflxhdxhd@gmail.com>
2021-06-18 01:13:22 +0900

    헤로쿠 배포 중...
    지도들이 안보임

--- server.js
+++ server.js
@@ -3,18 +3,13 @@ const app = express();
 
 app.use('/assets',express.static(__dirname + "/assets/"));
 
-
-
-
 app.get('/',function(req, res) {
     //__dirname 은 요청하고자 하는 파일의 경로(최상위 디렉토리~현재 디렉토리)를 단축시켜주는 절대경로 식별자
     res.sendFile(__dirname + "/index.html");
 })
 
-const PORT = process.env.PORT
-app.listen(PORT)
 
-app.listen(3000,()=>{
+app.listen(8080,()=>{
     console.log("server is loading :)");
 })
 
