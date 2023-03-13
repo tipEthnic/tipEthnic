d38647fb8d88e928eaaff8f45f50ba43856aafaf
or.llon <dhflxhdxhd@gmail.com>
2023-03-13 21:09:00 +0900

    Fix : error 해결 ing

---
+++ assets/js/App.js
@@ -0,0 +1,15 @@
+import {Button} from "./Button.js"
+
+
+class App{
+    constructor(){
+
+    }
+
+    play(){
+        Button.clickSenderBtn();
+    }
+}
+
+const app = new App();
+app.play();
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express'); //모듈 불러옴
 const app = express();
+const path = require('path');
 
-app.use('/assets',express.static(__dirname + "/assets/"));
+app.use(express.static("assets"));
 
-app.get('/',function(req, res) {
-    //__dirname 은 요청하고자 하는 파일의 경로(최상위 디렉토리~현재 디렉토리)를 단축시켜주는 절대경로 식별자
-    res.sendFile(__dirname + "/assets/html/index.html");
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + "/assets/views/index.html")
 })
 
 const PORT = process.env.PORT || 3000
