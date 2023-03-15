c19250a940a9f189e3975fefbe9871bc10893d60
or.llon <dhflxhdxhd@gmail.com>
2023-03-15 12:04:30 +0900

    Rename : main to Popup 파일 이름 변경

--- assets/js/Button.js
+++ assets/js/Button.js
@@ -34,21 +34,7 @@ class Button {
 
 }
 
-function saveLatLong() {
-    localStorage.setItem('receiver_latitude_y', localStorage.getItem('coords_lat'));
-    localStorage.setItem(
-        'receiver_longitude_x',
-        localStorage.getItem('coords_lon')
-    );
-    setTimeout(() =>{
-        receiver_place = localStorage.getItem("receiver_name");
-        //location.reload();
-        window
-            .document
-            .querySelector("#receiver")
-            .value = "현재 위치";
-    }, 500);
-}
+
 
 
 
--- assets/js/Popup.js
+++ assets/js/Popup.js
@@ -2,6 +2,21 @@
 // const maginot_check = document.getElementById("maginot").value;
 
 
+function saveLatLong() {
+    localStorage.setItem('receiver_latitude_y', localStorage.getItem('coords_lat'));
+    localStorage.setItem(
+        'receiver_longitude_x',
+        localStorage.getItem('coords_lon')
+    );
+    setTimeout(() =>{
+        receiver_place = localStorage.getItem("receiver_name");
+        //location.reload();
+        window
+            .document
+            .querySelector("#receiver")
+            .value = "현재 위치";
+    }, 500);
+}
 
 function searchSender() {
     window.open(
