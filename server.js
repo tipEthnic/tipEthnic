66099de523a9ec89cb36f11f119ea1a4a0b8b30a
or.llon <dhflxhdxhd@gmail.com>
2023-05-16 18:48:11 +0900

    Fix : 작동 안되는 코드 고치고...

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express'); //모듈 불러옴
+const express = require('express');
 const app = express();
-const path = require('path');
 
-app.use(express.static("assets"));
+app.use('/assets',express.static(__dirname + "/assets/"));
 
-app.get('/', function(req,res) {
-    res.sendFile(__dirname + "/assets/views/index.html")
+app.set('port',process.env.port||8080);
+
+
+app.get('/',function(req, res) {
+    //__dirname 은 요청하고자 하는 파일의 경로(최상위 디렉토리~현재 디렉토리)를 단축시켜주는 절대경로 식별자
+    res.sendFile(__dirname + "/index.html");
 })
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT,()=>{
-    console.log("server is loading :)" );
+
+app.listen(8080,()=>{
+    console.log("server is loading :)");
 })
+
